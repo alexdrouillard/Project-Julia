3b937528132ce85bfbbc7beae68929228453bbc5
Kevin <kevinchencode@gmail.com>
2017-03-20 23:58:40 -0400

    test multiply

--- test_m1_main.s
+++ test_m1_main.s
@@ -58,6 +58,25 @@ main:
 	addi sp, sp, 4 
 
 	call print_float
+	
+	movia r4, 0x41200000 	
+	movia r5, 0x40a00000
+	movia r6, 0x40a00000
+	movia r7, 0x40a00000
+
+	movia sp, sp_init
+	
+	call complex_divide
+	mov r4, r2
+	addi sp, sp, -4
+	stw r3, 0(sp)
+
+	call print_float
+
+	ldw r4, 0(sp)
+	addi sp, sp, 4 
+
+	call print_float
 	looper:
 		br looper
 
