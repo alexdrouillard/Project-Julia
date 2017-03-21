52f1084ce12eb1d2f61c36466fe917da52e5622e
Kevin <kevinchencode@gmail.com>
2017-03-20 23:51:47 -0400

    fuck this

--- test_m1_main.s
+++ test_m1_main.s
@@ -21,48 +21,22 @@ main:
 
 	call print_float
 
-	movia r4, 0x5
-	movia r5, 0x6
-	movia r6, 0x4
-	movia r7, 0x1
+	movia r4, 0x41200000 	
+	movia r5, 0x00000000
+	movia r6, 0x40a00000
+	movia r7, 0x40a00000
 
+	movia sp, sp_init
+	
 	call complex_subtract
-
-	mov r16, r3
-
-	call print_float
-
-	mov r2, r16
+	mov r4, r2
+	addi sp, sp, -4
+	stw r3, 0(sp)
 
 	call print_float
 
-	movia r4, 0x5
-	movia r5, 0x6
-	movia r6, 0x4
-	movia r7, 0x1
-
-	call complex_multiply
-
-	mov r16, r3
-
-	call print_float
-
-	mov r2, r16
-
-	call print_float
-
-	movia r4, 0x5
-	movia r5, 0x6
-	movia r6, 0x4
-	movia r7, 0x1
-
-	call complex_divide
-
-	mov r16, r3
-
-	call print_float
-
-	mov r2, r16
+	ldw r4, 0(sp)
+	addi sp, sp, 4 
 
 	call print_float
 
