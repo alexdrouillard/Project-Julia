99d93191c41cd3da11140834afdc5241aa979325
Kevin <kevinchencode@gmail.com>
2017-04-02 17:13:42 -0400

    changed main to test new function

--- test_m1_main.s
+++ test_m1_main.s
@@ -2,81 +2,20 @@
 
 .global main
 main:
-	movia r4, 0x40a00000 	
-	movia r5, 0x40a00000
-	movia r6, 0x40a00000
-	movia r7, 0x40a00000
+	movi r4, 0
+	movi r5, 0
 
-	movia sp, sp_init
-	
-	call complex_add
+	call iterate
 	mov r4, r2
-	addi sp, sp, -4
-	stw r3, 0(sp)
+	mov r5, r3
+	call print_complex
 
-	call print_float
+	movia r4, 0x40000000
+	movia r4, 0x40000000
 
-	ldw r4, 0(sp)
-	addi sp, sp, 4 
-
-	call print_float
-
-	movia r4, 0x41200000 	
-	movia r5, 0x00000000
-	movia r6, 0x40a00000
-	movia r7, 0x40a00000
-
-	movia sp, sp_init
-	
-	call complex_subtract
+	call iterate
 	mov r4, r2
-	addi sp, sp, -4
-	stw r3, 0(sp)
-
-	call print_float
-
-	ldw r4, 0(sp)
-	addi sp, sp, 4 
-
-	call print_float
-
-	movia r4, 0x41200000 	
-	movia r5, 0x40a00000
-	movia r6, 0x40a00000
-	movia r7, 0x40a00000
-
-	movia sp, sp_init
-	
-	call complex_multiply
-	mov r4, r2
-	addi sp, sp, -4
-	stw r3, 0(sp)
-
-	call print_float
-
-	ldw r4, 0(sp)
-	addi sp, sp, 4 
-
-	call print_float
-	
-	movia r4, 0x41200000 	
-	movia r5, 0x40a00000
-	movia r6, 0x40a00000
-	movia r7, 0x40a00000
-
-	movia sp, sp_init
-	
-	call complex_divide
-	mov r4, r2
-	addi sp, sp, -4
-	stw r3, 0(sp)
-
-	call print_float
-
-	ldw r4, 0(sp)
-	addi sp, sp, 4 
-
-	call print_float
-	looper:
-		br looper
-
+	mov r5, r3
+	call print_complex
+looper:
+	br looper
