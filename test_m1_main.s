7d2ba5c748e1271725383e6ab9b48a5f5d114111
alexdrouillard <alex.drouillard1@gmail.com>
2017-04-04 09:31:14 -0400

    real main

--- test_m1_main.s
+++ test_m1_main.s
@@ -1,43 +1,18 @@
 .equ sp_init, 0x017FFF80
-.equ left, 0xC0C00000 #-6.0
-.equ right, 0x40C00000 #6.0
-.equ top, 0x40800000 #4.0 GPA sad bois
-.equ bot, 0xC0800000 #-4.0 
+.equ left, 0x40000000 #-2.0
+.equ right, 0xC0000000 #2.0
+.equ top, 0x3f800000 #1.0 GPA sad bois
+.equ bot, 0xbf800000 #-1.0 
 .equ width, 0x43A00000 #320.0 in hex
 .equ height, 0x43700000 #240.0 in hex
 
 .global main
 main:
-	movia sp, sp_init
-	movi r4, 0
-	movi r5, 0
 
-	call iterate
-	mov r4, r2
-	mov r5, r3
-	call print_int
+    movia sp, sp_init
 
-	movia r4, 0x40000000
-	movia r5, 0x40000000
 
-	call iterate
-	mov r4, r2
-	mov r5, r3
-	call print_int
+    call draw_set
 
-    #want to write blue to a bunch of pixels
-   	mov r7, r0 #angle
-	movui r8, 200 #end loop
-        movui r9, 100
-	draw_rainbow: 
-		mov r4, r7
-		call hsv_to_rgb
-		mov   r6, r2 
-   		mov   r4, r9
-                movui r5, 100
-		addi r7, r7, 1
- 	   	call draw_pixel
-                addi r9, r9, 1
-		bne r8, r7, draw_rainbow  
 looper:
 	br looper
