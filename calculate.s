1d4b269b7bc18a7382095afd20d4f441dfde9d52
alexdrouillard <alex.drouillard1@gmail.com>
2017-04-09 21:29:05 -0400

    improve draw speed by 1s

--- calculate.s
+++ calculate.s
@@ -6,36 +6,40 @@
 .global iterate
 iterate:
 	addi sp, sp, -32
-	stw  r16, 0(sp)
-	stw  r17, 4(sp)
-	stw  r18, 8(sp)
-	stw  r4,  12(sp)
-	stw  r5,  16(sp)
-    stw  ra,  20(sp)
+        stw  ra,  20(sp)
 
-	movia r17, maxiter
-	movia r18, threshold
-	movia r16, 0
+	movia r12, maxiter
+	movia r13, threshold
+	movia r11, 0
 
 	iterate_loop:
-		beq r17, r16, iterate_loop_done
+		beq r12, r11, iterate_loop_done
 		call complex_magnitude
-		bgt r2, r18, iterate_loop_done
-		call equation
+		bgt r2, r13, iterate_loop_done
+
+                # CALCULATE
+                mov  r6, r4
+                mov  r7, r5
+                call complex_multiply
+
+                movia r4, 0xbf4ccccd
+                movia r5, 0x3e1fbe77
+
+                mov r6, r2
+                mov r7, r3
+
+                call complex_add
+
+                #calculate
 		mov  r4, r2
 		mov  r5, r3
-        addi r16, r16, 1
+                addi r11, r11, 1
 		br iterate_loop
 	iterate_loop_done:
 
-	mov r2, r16
-    
-    ldw  ra,  20(sp)
-	ldw  r5,  16(sp)
-	ldw  r4,  12(sp)
-	ldw  r18, 8(sp)
-	ldw  r17, 4(sp)
-	ldw  r16, 0(sp)
+	mov r2, r11
+        
+        ldw ra, 20(sp) 
 	addi sp, sp, 32
 	ret
 
