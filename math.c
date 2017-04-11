ef3f94323cb1960da1ca9d1b3981c5ea77534069
Kevin <kevinchencode@gmail.com>
2017-04-11 04:23:31 -0400

    added string to float function

--- math.c
+++ math.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 float float_add(float a, float b){
 	return a + b;
@@ -28,6 +29,26 @@ float int_to_float(int a) {
 
 int float_to_int(float a) {
 	return (int)a;
+}
+
+float string_to_float(const char* s){
+  float rez = 0, fact = 1;
+  if (*s == '-'){
+    s++;
+    fact = -1;
+  };
+  for (int point_seen = 0; *s; s++){
+    if (*s == '.'){
+      point_seen = 1; 
+      continue;
+    };
+    int d = *s - '0';
+    if (d >= 0 && d <= 9){
+      if (point_seen) fact /= 10.0f;
+      rez = rez * 10.0f + (float)d;
+    };
+  };
+  return rez * fact;
 }
 
 void print_float(float a) {
